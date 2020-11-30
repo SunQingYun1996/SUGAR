24d6faa5945b92f8d3be80d4941ab0f3d3b28a2c
SunQingYun1996 <43441277+SunQingYun1996@users.noreply.github.com>
2020-11-30 16:36:52 +0800

    Update env2.py

--- env2.py
+++ env2.py
@@ -18,7 +18,6 @@ class GNN_env1(object):
 
     def get_observation(self):
         select_num = int(self.k * self.n_features)
-        # observation = [1] * select_num + [0] * (self.n_features - select_num)
         observation = select_num
         return observation
 
@@ -31,23 +30,16 @@ class GNN_env1(object):
                 self.k -= self.action_value
 
         eva_loss, eva_acc, _, rl_reward, _, _, _, _ = net.evaluate(test_x, test_dsi, test_sadj, test_t, test_t_mi, test_mask, self.k)
-        # print('rl_reward',rl_reward)
-        # print(rl_reward.all)
-        # eva_acc = round(eva_acc, 3)
         s_ = self.get_observation()
         # print(s_)
         s_ = np.array(s_)
         # reward function
         if rl_reward > last_acc:
             reward = 1
-            # reward = 1 - eva_loss
             done = True
-            # s_ = 'terminal'
         elif rl_reward < last_acc:
             reward = -1
-            # reward = 0 - eva_loss
             done = True
-            # s_ = 'terminal'
         else:
             reward = 0
             done = True
